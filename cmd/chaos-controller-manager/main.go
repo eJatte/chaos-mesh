338ace3bea1d89fb1a8ca5208287cb28e69fe5bf
Elias <c16eje@cs.umu.se>
2021-01-29 12:47:32 +0100

    got hello world example to work

--- cmd/chaos-controller-manager/main.go
+++ cmd/chaos-controller-manager/main.go
@@ -48,6 +48,8 @@ import (
 	_ "github.com/chaos-mesh/chaos-mesh/controllers/podchaos/podkill"
 	_ "github.com/chaos-mesh/chaos-mesh/controllers/stresschaos"
 	_ "github.com/chaos-mesh/chaos-mesh/controllers/timechaos"
+	_ "github.com/chaos-mesh/chaos-mesh/controllers/helloworldchaos"
+
 
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
