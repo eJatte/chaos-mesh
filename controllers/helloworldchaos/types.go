74dd2231e6d6848107433a7f1010616b317ce823
Elias <c16eje@cs.umu.se>
2021-02-01 17:47:20 +0100

    permissions and trying to create container in chaosmesh

--- controllers/helloworldchaos/types.go
+++ controllers/helloworldchaos/types.go
@@ -2,7 +2,8 @@ package helloworldchaos
 
 import (
 	"context"
-
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 
@@ -19,6 +20,56 @@ type endpoint struct {
 func (e *endpoint) Apply(ctx context.Context, req ctrl.Request, chaos v1alpha1.InnerObject) error {
 	e.Log.Info("Hello World!")
 	e.Log.Info("Ostpojke")
+	var p int32 = 3
+	var user int64 = 0
+	var selectorMap map[string]string
+	selectorMap = make(map[string]string)
+	selectorMap["matchLabels"] = "app: hello-kubernetes"
+
+	var labelMap map[string]string
+	labelMap = make(map[string]string)
+	labelMap["app"] = "hello-kubernetes"
+
+	replicationController := v1.ReplicationController{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "hello-kubernetes",
+			Namespace: "chaos-testing",
+		},
+		Spec: v1.ReplicationControllerSpec{
+			Replicas: &p,
+			Selector: selectorMap,
+			Template: &v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labelMap,
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:  "hello-kubernetes",
+							Image: "paulbouwer/hello-kubernetes:1.8",
+							Ports: []v1.ContainerPort{
+								{
+									ContainerPort: 8080,
+								},
+							},
+							SecurityContext: &v1.SecurityContext{
+								RunAsUser: &user,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	err := e.Create(ctx, &replicationController)
+	if err != nil {
+		e.Log.Error(err, "Failed to create replication controller")
+		return err
+	}
 	return nil
 }
 
