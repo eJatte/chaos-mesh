98e9bdd4dd346bf5a03e431f327e0eac1373e747
Elias <c16eje@cs.umu.se>
2021-02-22 15:04:11 +0100

    working example of creating a file then deleting it via remote execution from within the chaos tool

--- pkg/chaosdaemon/helloworld_server.go
+++ pkg/chaosdaemon/helloworld_server.go
@@ -18,13 +18,85 @@ func (s *DaemonServer) ExecHelloWorldChaos(ctx context.Context, req *pb.ExecHell
 		return nil, err
 	}
 
-	cmd := bpm.DefaultProcessBuilder("sh", "-c", fmt.Sprintf("echo 'hello' `hostname`")).
+	log.Info("Executing ls")
+	cmd := bpm.DefaultProcessBuilder("sh", "-c", fmt.Sprintf("ls")).
 		SetNS(pid, bpm.PidNS).
 		SetContext(ctx).
 		Build()
 	out, err := cmd.Output()
 	if err != nil {
-		log.Error(err, "Failed to execute command")
+		log.Error(err, "Failed to execute ls")
+		return nil, err
+	}
+	if len(out) != 0 {
+		log.Info("cmd output", "output", string(out))
+	}
+
+	log.Info("Executing echo hello world to file")
+	cmd = bpm.DefaultProcessBuilder("sh", "-c", fmt.Sprintf("echo \"hello world!\" >> hello.txt")).
+		SetNS(pid, bpm.PidNS).
+		SetContext(ctx).
+		Build()
+	out, err = cmd.Output()
+	if err != nil {
+		log.Error(err, "Failed to execute echo")
+		return nil, err
+	}
+	if len(out) != 0 {
+		log.Info("cmd output", "output", string(out))
+	}
+
+	log.Info("Executing ls")
+	cmd = bpm.DefaultProcessBuilder("sh", "-c", fmt.Sprintf("ls")).
+		SetNS(pid, bpm.PidNS).
+		SetContext(ctx).
+		Build()
+	out, err = cmd.Output()
+	if err != nil {
+		log.Error(err, "Failed to execute ls")
+		return nil, err
+	}
+	if len(out) != 0 {
+		log.Info("cmd output", "output", string(out))
+	}
+
+	log.Info("Executing cat")
+	cmd = bpm.DefaultProcessBuilder("sh", "-c", fmt.Sprintf("cat hello.txt")).
+		SetNS(pid, bpm.PidNS).
+		SetContext(ctx).
+		Build()
+
+	out, err = cmd.Output()
+	if err != nil {
+		log.Error(err, "Failed to execute cat")
+		return nil, err
+	}
+	if len(out) != 0 {
+		log.Info("cmd output", "output", string(out))
+	}
+
+	log.Info("Executing rm")
+	cmd = bpm.DefaultProcessBuilder("sh", "-c", fmt.Sprintf("rm hello.txt")).
+		SetNS(pid, bpm.PidNS).
+		SetContext(ctx).
+		Build()
+	out, err = cmd.Output()
+	if err != nil {
+		log.Error(err, "Failed to execute rm")
+		return nil, err
+	}
+	if len(out) != 0 {
+		log.Info("cmd output", "output", string(out))
+	}
+
+	log.Info("Executing ls")
+	cmd = bpm.DefaultProcessBuilder("sh", "-c", fmt.Sprintf("ls")).
+		SetNS(pid, bpm.PidNS).
+		SetContext(ctx).
+		Build()
+	out, err = cmd.Output()
+	if err != nil {
+		log.Error(err, "Failed to execute ls")
 		return nil, err
 	}
 	if len(out) != 0 {
