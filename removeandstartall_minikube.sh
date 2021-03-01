3fc16a16d0484f8a6157e9cc5e81edde772d59dd
Elias <c16eje@cs.umu.se>
2021-03-01 15:14:37 +0100

    added roles to startup script

--- removeandstartall_minikube.sh
+++ removeandstartall_minikube.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 minikube delete
 minikube start
+kubectl apply -f service_account_cr.yaml
+kubectl apply -f service_account_crb.yaml
 ./createandpushimage.sh
 ./loadimages_minikube.sh
 ./installchaosmesh_helm.sh
