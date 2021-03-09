388452ba9301e0e6ae720b906c4485fe0c86b45d
Elias <c16eje@cs.umu.se>
2021-03-09 15:22:24 +0100

    permissions and such for folders and users etc

--- scenarios/deletefile/apply.sh
+++ scenarios/deletefile/apply.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-minikube ssh 'sudo mkdir /mnt/data && cd /mnt/data/ && sudo sh -c "echo 'hello boys' > hello.txt"'
+minikube ssh 'sudo mkdir /mnt/data && sudo chown -R :1234 /mnt/data/ && sudo chmod g+rw /mnt/data'
 kubectl apply -f deployment.yaml
